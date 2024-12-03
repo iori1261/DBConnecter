c0a5ce576c04898fc91fafd78ad44cda86d4c85e
236137 <236137@KCSF0971>
2024-12-03 09:19:15 +0900

    プロジェクト ファイルを追加します。

---
+++ DBConnecter/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace DBConnecter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.id = new System.Windows.Forms.Label();
+            this.name = new System.Windows.Forms.Label();
+            this.role = new System.Windows.Forms.Label();
+            this.lblId1 = new System.Windows.Forms.Label();
+            this.lblId2 = new System.Windows.Forms.Label();
+            this.lblId3 = new System.Windows.Forms.Label();
+            this.lblName1 = new System.Windows.Forms.Label();
+            this.lblName2 = new System.Windows.Forms.Label();
+            this.lblName3 = new System.Windows.Forms.Label();
+            this.lblRole1 = new System.Windows.Forms.Label();
+            this.lblRole2 = new System.Windows.Forms.Label();
+            this.lblRole3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // id
+            // 
+            this.id.Font = new System.Drawing.Font("MS UI Gothic", 15F);
+            this.id.Location = new System.Drawing.Point(107, 57);
+            this.id.Name = "id";
+            this.id.Size = new System.Drawing.Size(100, 23);
+            this.id.TabIndex = 0;
+            this.id.Text = "ID";
+            // 
+            // name
+            // 
+            this.name.Font = new System.Drawing.Font("MS UI Gothic", 15F);
+            this.name.Location = new System.Drawing.Point(280, 57);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(100, 23);
+            this.name.TabIndex = 1;
+            this.name.Text = "名前";
+            // 
+            // role
+            // 
+            this.role.Font = new System.Drawing.Font("MS UI Gothic", 15F);
+            this.role.Location = new System.Drawing.Point(494, 57);
+            this.role.Name = "role";
+            this.role.Size = new System.Drawing.Size(100, 23);
+            this.role.TabIndex = 2;
+            this.role.Text = "ロール";
+            // 
+            // lblId1
+            // 
+            this.lblId1.Font = new System.Drawing.Font("MS UI Gothic", 15F);
+            this.lblId1.Location = new System.Drawing.Point(107, 135);
+            this.lblId1.Name = "lblId1";
+            this.lblId1.Size = new System.Drawing.Size(121, 23);
+            this.lblId1.TabIndex = 3;
+            this.lblId1.Text = "1人目のID";
+            // 
+            // lblId2
+            // 
+            this.lblId2.Font = new System.Drawing.Font("MS UI Gothic", 15F);
+            this.lblId2.Location = new System.Drawing.Point(107, 176);
+            this.lblId2.Name = "lblId2";
+            this.lblId2.Size = new System.Drawing.Size(121, 23);
+            this.lblId2.TabIndex = 4;
+            this.lblId2.Text = "2人目のID";
+            // 
+            // lblId3
+            // 
+            this.lblId3.Font = new System.Drawing.Font("MS UI Gothic", 15F);
+            this.lblId3.Location = new System.Drawing.Point(107, 218);
+            this.lblId3.Name = "lblId3";
+            this.lblId3.Size = new System.Drawing.Size(121, 23);
+            this.lblId3.TabIndex = 5;
+            this.lblId3.Text = "3人目のID";
+            // 
+            // lblName1
+            // 
+            this.lblName1.Font = new System.Drawing.Font("MS UI Gothic", 15F);
+            this.lblName1.Location = new System.Drawing.Point(280, 132);
+            this.lblName1.Name = "lblName1";
+            this.lblName1.Size = new System.Drawing.Size(163, 26);
+            this.lblName1.TabIndex = 6;
+            this.lblName1.Text = "1人目の名前";
+            // 
+            // lblName2
+            // 
+            this.lblName2.Font = new System.Drawing.Font("MS UI Gothic", 15F);
+            this.lblName2.Location = new System.Drawing.Point(280, 176);
+            this.lblName2.Name = "lblName2";
+            this.lblName2.Size = new System.Drawing.Size(163, 23);
+            this.lblName2.TabIndex = 7;
+            this.lblName2.Text = "2人目の名前";
+            // 
+            // lblName3
+            // 
+            this.lblName3.Font = new System.Drawing.Font("MS UI Gothic", 15F);
+            this.lblName3.Location = new System.Drawing.Point(280, 218);
+            this.lblName3.Name = "lblName3";
+            this.lblName3.Size = new System.Drawing.Size(163, 23);
+            this.lblName3.TabIndex = 8;
+            this.lblName3.Text = "3人目の名前";
+            // 
+            // lblRole1
+            // 
+            this.lblRole1.Font = new System.Drawing.Font("MS UI Gothic", 15F);
+            this.lblRole1.Location = new System.Drawing.Point(494, 135);
+            this.lblRole1.Name = "lblRole1";
+            this.lblRole1.Size = new System.Drawing.Size(171, 26);
+            this.lblRole1.TabIndex = 9;
+            this.lblRole1.Text = "1人目のロール";
+            // 
+            // lblRole2
+            // 
+            this.lblRole2.Font = new System.Drawing.Font("MS UI Gothic", 15F);
+            this.lblRole2.Location = new System.Drawing.Point(494, 176);
+            this.lblRole2.Name = "lblRole2";
+            this.lblRole2.Size = new System.Drawing.Size(171, 23);
+            this.lblRole2.TabIndex = 10;
+            this.lblRole2.Text = "2人目のロール";
+            // 
+            // lblRole3
+            // 
+            this.lblRole3.Font = new System.Drawing.Font("MS UI Gothic", 15F);
+            this.lblRole3.Location = new System.Drawing.Point(494, 218);
+            this.lblRole3.Name = "lblRole3";
+            this.lblRole3.Size = new System.Drawing.Size(161, 23);
+            this.lblRole3.TabIndex = 11;
+            this.lblRole3.Text = "3人目のロール";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("MS UI Gothic", 15F);
+            this.button1.Location = new System.Drawing.Point(308, 348);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 35);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "読込";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(746, 453);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblRole3);
+            this.Controls.Add(this.lblRole2);
+            this.Controls.Add(this.lblRole1);
+            this.Controls.Add(this.lblName3);
+            this.Controls.Add(this.lblName2);
+            this.Controls.Add(this.lblName1);
+            this.Controls.Add(this.lblId3);
+            this.Controls.Add(this.lblId2);
+            this.Controls.Add(this.lblId1);
+            this.Controls.Add(this.role);
+            this.Controls.Add(this.name);
+            this.Controls.Add(this.id);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label id;
+        private System.Windows.Forms.Label name;
+        private System.Windows.Forms.Label role;
+        private System.Windows.Forms.Label lblId1;
+        private System.Windows.Forms.Label lblId2;
+        private System.Windows.Forms.Label lblId3;
+        private System.Windows.Forms.Label lblName1;
+        private System.Windows.Forms.Label lblName2;
+        private System.Windows.Forms.Label lblName3;
+        private System.Windows.Forms.Label lblRole1;
+        private System.Windows.Forms.Label lblRole2;
+        private System.Windows.Forms.Label lblRole3;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ DBConnecter/Form1.cs
@@ -0,0 +1,61 @@
+﻿using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DBConnecter
+{
+    public partial class Form1 : Form
+    {
+        //接続情報
+        String connectionString;
+        List<Label> lblIds;
+        List<Label> lblNames;
+        List<Label> lblRoles;
+
+        public Form1()
+        {
+            InitializeComponent();
+            //接続情報の設定
+            connectionString = "Server=localhost;Database=Major;UserID=root;Password=kcsf;Pooling=true;";
+            lblIds = new List<Label> { lblId1, lblId2, lblId3 };
+            lblNames = new List<Label> { lblName1, lblName2, lblName3 };
+            lblRoles = new List<Label> { lblRole1, lblRole2, lblRole3 };
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //DBに接続してユーザ情報を取得
+            using (MySqlConnection con = new MySqlConnection(connectionString))
+            {
+                con.Open();
+
+                //クエリの実行
+                using (MySqlCommand cmd = new MySqlCommand("SELECT * FROM Users",con))
+                {
+                    //javaにおける「ResultSe rs = stmt ExecuteQuery()」
+                    using(MySqlDataReader reader = cmd.ExecuteReader()) 
+                    {
+                        int index = 0;
+                        while (reader.Read() && index < lblIds.Count)
+                        {
+                            lblIds[index].Text = reader["id"].ToString();
+                            lblNames[index].Text = reader["name"].ToString();
+                            lblRoles[index].Text = reader["role"].ToString();
+
+                            index++;
+                        }
+                        reader.Close();
+                    }
+                }
+            }
+        }
+    }
+}
